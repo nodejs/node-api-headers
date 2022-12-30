0834c30961cec82d25cb0736ec1216b18a1d3cfd
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2022-12-30 23:47:57 +0100

    Use latest release from nodejs.org releases

--- scripts/update-headers.js
+++ scripts/update-headers.js
@@ -2,18 +2,40 @@ const { createWriteStream } = require('fs');
 const { Readable } = require('stream');
 const { finished } = require('stream/promises');
 const { resolve } = require('path');
+const { parseArgs } = require('util')
 
-const files = ['js_native_api_types.h', 'js_native_api.h', 'node_api_types.h', 'node_api.h'];
-
-const commit = process.argv[2] ?? `${process.version.substring(0,3)}.x`;
-
-console.log(`Using commit ${commit}:`);
+async function getLatestReleaseVersion() {
+    const response = await fetch('https://nodejs.org/download/release/index.json');
+    const json = await response.json();
+    return json[0].version;
+}
 
 async function main() {
+    const { values: { tag, verbose } } = parseArgs({
+        options: {
+            tag: {
+                type: "string",
+                short: "t",
+                default: await getLatestReleaseVersion()
+            },
+            verbose: {
+                type: "boolean",
+                short: "v",
+            },
+        },
+    });
+
+    console.log(`Update headers from nodejs/node tag ${tag}`);
+
+    const files = ['js_native_api_types.h', 'js_native_api.h', 'node_api_types.h', 'node_api.h'];
+
     for (const filename of files) {
-        const url = `https://raw.githubusercontent.com/nodejs/node/${commit}/src/${filename}`;
+        const url = `https://raw.githubusercontent.com/nodejs/node/${tag}/src/${filename}`;
         const path = resolve(__dirname, '..', 'include', filename);
-        console.log(`  ${url} -> ${path}`);
+
+        if (verbose) {
+            console.log(`  ${url} -> ${path}`);
+        }
 
         const response = await fetch(url);
         if (!response.ok) {
